fa8d203c8bd0251f8c50ca2e157c149a59ec2ecf
Karleen Kennos <karleenk@si-a5.nsk.21-school.ru>
2024-10-21 16:44:14 +0700

    view

---
+++ A1_Maze_cpp/maze.h
@@ -0,0 +1,33 @@
+#ifndef MAZE_H_
+#define MAZE_H_
+
+#include <list>
+#include <iostream>
+
+namespace s21 {
+
+class Maze {
+ public:
+    Maze(int row, int col);
+    Maze(int row, int col, int bias);
+    int getRow() {return row_; };
+    int getCol() {return col_; };
+
+    std::vector<std::vector<int>> getRightWalls() { return rightWalls_; };
+    std::vector<std::vector<int>> getBottomWalls() { return bottomWalls_; };
+    std::vector<std::vector<int>> getPath() { return path_; };
+    void FindPath(int startPosX, int startPosY, int endPosX, int endPosY);
+
+ private:
+    int row_;
+    int col_;
+    std::vector<std::vector<int>> rightWalls_;
+    std::vector<std::vector<int>> bottomWalls_;
+    std::vector<std::vector<int>> path_;
+
+    void RandomValue(); //Для теста отрисовки
+};
+
+}  // namespace s21
+
+#endif  // MAZE_H_
