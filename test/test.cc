cb529351e6a3ad399a0796a04558146e87073f19
Walt Latesha <waltlate@si-a3.nsk.21-school.ru>
2024-10-23 11:39:34 +0700

    create makefile, folder test and alt manual

---
+++ test/test.cc
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+#include "../A1_Maze_cpp/maze.h"
+
+TEST(Maze, affins_move_1) {
+
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
